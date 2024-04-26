b660d01eb8754195eb0c9b2472964ce4bda55545
Diogo Rodrigues <diogogamerpudim@gmail.com>
2024-04-25 22:37:46 -0300

    fixing slides

--- slideshow.js
+++ slideshow.js
@@ -1,8 +1,7 @@
 var imageUrls = [
-    'img/carrosel-img1.svg',
-    'img/carrosel-img2.svg',
-    'img/carrosel-img3.svg',    
-    'img/carrosel-img4.svg'
+    'carrosel-img1.svg',
+    'carrosel-img2.svg',  
+    'carrosel-img4.svg', 
 ]
 
 var currentIndex = 0;
